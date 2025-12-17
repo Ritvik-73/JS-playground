0b9bd6587e9e070d12670be34c8f2dc65d927d49
Ritvik-73 <khuranaritvik73@gmail.com>
2025-12-17 21:36:20 +0530

    DAY1

---
+++ JS-playground/DAY1/script.js
@@ -0,0 +1,25 @@
+console.log("JS connected sucessfully!!")
+
+let name = "Ritvik Khurana"
+let age = 17
+
+console.log("user name:", name)
+console.log("user age:", age)
+
+let nextYearAge = age + 1
+console.log("Age next year will be:", nextYearAge)
+
+sum = 0
+for (let i = 1; i <= 5; i++){
+    console.log(i)
+    sum = sum + i
+}
+console.log("Sum is:", sum)
+
+
+alert(`JS has been connected sucessfully!!
+Welcome ${name}, your age is ${age}`)
+
+prompt("How are you " + name + "?")
+confirm(`It was nice talking to you, have a great day ahead!
+Are you sure you want to proceed?`)
---
+++ JS-playground/README.md
@@ -0,0 +1,2 @@
+# JS-playground
+This repo is for my practice of javascript, building something myself, implementing what i have learnt till now.
