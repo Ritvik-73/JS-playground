15938be77eed393fb524a0a39848411dd90a49d0
Ritvik-73 <khuranaritvik73@gmail.com>
2025-12-20 19:55:29 +0530

    DAY-6

---
+++ DAY-6[To-Do list](Improved)/script.js
@@ -0,0 +1,59 @@
+let messages = [];
+
+input=document.getElementById("InputTxt")
+butn=document.getElementById("butn")
+list=document.getElementById("list")
+clickCounter=document.getElementById("butnClicked")
+
+input.addEventListener("keydown",function(event){
+    if(event.key==="Enter"){
+        event.preventDefault();
+        butn.click();
+    }
+})
+
+butn.addEventListener("click",function(){
+    inputValue = input.value.trim();
+
+    if(inputValue === "")return;
+
+    messages.push({
+        text:inputValue,
+        completed:false
+    });
+    input.value = "";
+
+    printMessages();
+})
+
+function printMessages(){
+    list.innerHTML = "";
+    for(let i=0;i<=messages.length -1;i++){
+        const li = document.createElement("li");
+        
+        const span = document.createElement("span");
+        span.innerText = messages[i].text;
+
+        if(messages[i].completed){
+            span.style.textDecoration = "line-through";
+        }
+
+        span.addEventListener("click",function(){
+            messages[i].completed = !messages[i].completed;
+            printMessages();
+        })
+
+        const deletebtn = document.createElement("button");
+        deletebtn.innerText = "❌";
+        deletebtn.addEventListener("click",function(){
+            messages.splice(i,1);
+            printMessages();
+        })
+
+        li.appendChild(span);
+        li.appendChild(deletebtn);
+        list.appendChild(li);
+    }
+}
+
+
